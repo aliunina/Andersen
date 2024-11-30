bd96e711ec08497d2a56c5d6f6afa1bd1e02fcbe
Polina Aliunina <aliunina@github.com>
2024-11-30 21:41:21 +0300

    add: spring application context

--- Task8/src/main/java/org/andersen/Main.java
+++ Task8/src/main/java/org/andersen/Main.java
@@ -1,40 +1,39 @@
 package org.andersen;
 
-import org.andersen.connection.ConnectionFactory;
 import org.andersen.dao.TicketDao;
 import org.andersen.dao.UserDao;
-import org.andersen.dao.impl.TicketDaoImpl;
-import org.andersen.dao.impl.UserDaoImpl;
 import org.andersen.model.*;
+import org.andersen.util.ApplicationContextConfigurationUtil;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "pass123";
-
     public static void main(String[] args) {
-        ConnectionFactory cf = new ConnectionFactory();
-        cf.connectToDatabase(USER, PASSWORD);
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(ApplicationContextConfigurationUtil.class);
+
         try {
-            UserDao userDao = new UserDaoImpl();
-            TicketDao ticketDao = new TicketDaoImpl();
+            UserDao userDao = ctx.getBean(UserDao.class);
+            TicketDao ticketDao = ctx.getBean(TicketDao.class);
+            DataSource dataSource = ctx.getBean(DataSource.class);
 
-            userDao.insertUser("new user");
-            ticketDao.insertTicket(4, TicketType.WEEK);
+//            userDao.insertUser("new user");
+//            ticketDao.insertTicket(4, TicketType.WEEK);
 
-            User user = userDao.selectUserById(3);
+            User user = userDao.selectUserById(4);
             System.out.println("Selected user: " + user);
             Ticket ticket = ticketDao.selectTicketById(4);
             System.out.println("Selected ticket: " + ticket);
 
 
-            List<Ticket> tickets = ticketDao.selectTicketsByUserId(4);
-            System.out.println(tickets);
-
-            ticketDao.updateTicketType(4, TicketType.YEAR);
-            userDao.deleteUser(3);
+//            List<Ticket> tickets = ticketDao.selectTicketsByUserId(4);
+//            System.out.println(tickets);
+//
+//            ticketDao.updateTicketType(4, TicketType.YEAR);
+//            userDao.deleteUser(3);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
--- Task8/src/main/java/org/andersen/dao/impl/UserDaoImpl.java
+++ Task8/src/main/java/org/andersen/dao/impl/UserDaoImpl.java
@@ -1,23 +1,29 @@
 package org.andersen.dao.impl;
 
-import org.andersen.connection.ConnectionFactory;
 import org.andersen.dao.UserDao;
 import org.andersen.model.User;
+import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
+@Component
 public class UserDaoImpl implements UserDao {
-    private Connection connection = ConnectionFactory.getConnection();
+    private final DataSource dataSource;
     private static final String SQL_INSERT_USER =
             "INSERT INTO public.\"User\"(id, name, creation_date) VALUES (DEFAULT, '%s', '%s')";
     private static final String SQL_SELECT_USER_BY_ID = "SELECT * FROM public.\"User\" WHERE id = %d";
     private static final String SQL_DELETE_USER_TICKETS = "DELETE FROM public.\"Ticket\" WHERE user_id = %d";
     private static final String SQL_DELETE_USER = "DELETE FROM public.\"User\" WHERE id = %d";
 
+    public UserDaoImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public void insertUser(String name) throws SQLException {
         String query = String.format(SQL_INSERT_USER, name, getCreationDate());
-        Statement statement = connection.createStatement();
+        Statement statement = dataSource.getConnection().createStatement();
         statement.executeUpdate(query);
         System.out.println("User inserted.");
     }
@@ -25,7 +31,7 @@ public class UserDaoImpl implements UserDao {
     @Override
     public User selectUserById(long id) throws SQLException {
         String query = String.format(SQL_SELECT_USER_BY_ID, id);
-        Statement statement = connection.createStatement();
+        Statement statement = dataSource.getConnection().createStatement();
 
         ResultSet res = statement.executeQuery(query);
         res.next();
@@ -36,7 +42,7 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void deleteUser(long id) throws SQLException {
-        Statement statement = connection.createStatement();
+        Statement statement = dataSource.getConnection().createStatement();
         String query1 = String.format(SQL_DELETE_USER_TICKETS, id);
         statement.executeUpdate(query1);
         String query2 = String.format(SQL_DELETE_USER, id);
